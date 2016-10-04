227937bac6d71bc3d6041edf003c6f66e7bc0a11
Andrey Ponomarenko <andrewponomarenko@yandex.ru>
2016-10-04 20:12:35 +0300

    Added LICENSE file.

--- pkg-abidiff.py
+++ pkg-abidiff.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-###############################################################
+#################################################################
 # Package ABI Diff 0.95
 # Verify API/ABI compatibility of Linux packages (RPM or DEB)
 #
@@ -21,7 +21,20 @@
 #  Elfutils
 #  G++
 #
-###############################################################
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License or
+# the GNU Lesser General Public License as published by the Free
+# Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License and the GNU Lesser General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+#################################################################
 import argparse
 import re
 import sys
