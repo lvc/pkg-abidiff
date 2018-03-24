63d96074eee4e7da4ef03d393764a05a61a57634
Andrey Ponomarenko <andrewponomarenko@yandex.ru>
2018-03-24 19:20:15 +0300

    Fixed license note

--- pkg-abidiff.py
+++ pkg-abidiff.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#################################################################
+#####################################################################
 # Package ABI Diff 0.97
 # Verify API/ABI compatibility of Linux packages (RPM or DEB)
 #
@@ -21,20 +21,21 @@
 #  Elfutils
 #  G++
 #
-# This program is free software: you can redistribute it and/or
-# modify it under the terms of the GNU General Public License or
-# the GNU Lesser General Public License as published by the Free
-# Software Foundation.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public
-# License and the GNU Lesser General Public License along with
-# this program. If not, see <http://www.gnu.org/licenses/>.
-#################################################################
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301 USA
+#####################################################################
 import argparse
 import re
 import sys
